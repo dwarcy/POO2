750713fbb42d4072a8df4d56199ac040086ac8f1
dwarcy <renata.b.silva2002@gmail.com>
2023-08-26 19:43:00 -0300

    Exercício
    Signed-off-by: dwarcy <renata.b.silva2002@gmail.com>

---
+++ src/Questao_Um/Celular.java
@@ -0,0 +1,6 @@
+package Questao_Um;
+
+public interface Celular {
+    public void fazLigacao();
+    public void tiraFoto();
+}
---
+++ src/Questao_Um/FabricanteCelular.java
@@ -0,0 +1,5 @@
+package Questao_Um;
+
+public interface FabricanteCelular {
+
+}
